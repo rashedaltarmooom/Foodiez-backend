d3cd37622ad7a2cbf90be37048059b5a321a7f8d
Rashed Altarmoom <rashed.al6armoom@gmail.com>
2022-06-08 19:33:16 +0300

    fixed signup

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const signup = require("./api/users/users.routes");
 const connectDB = require("./db/database");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/users", signup);
 
 connectDB();
 
