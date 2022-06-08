59ffdd706e8238006459e989598388f3b8cb7b68
SalwaBaqer <s.baqer@hotmail.com>
2022-06-08 16:48:22 +0300

    Add Category list routes and controllers

---
+++ api/category/category.controllers.js
@@ -0,0 +1,10 @@
+const Category = require("../../db/models/Category");
+
+exports.categoriesGet = async (req, res, next) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    next(error);
+  }
+};
